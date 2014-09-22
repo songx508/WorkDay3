060c92bd005f06081e2e98d1132bbedef3e4c0a1
idsc3101 <idsc3101@gmail.com>
2014-09-22 15:21:40 -0500

    completed

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -4,12 +4,23 @@ Public Class Form1
 
     Private Sub btnFindLarger_Click(sender As System.Object, e As System.EventArgs) Handles btnFindLarger.Click
         Dim num1, num2, largerNum As Double
-
+        '0s are automatically assigned to num1 and num2
         ' Modify the program such that it does not crash 
         ' if the user types incorrect input
 
         Dim st1 As String = txtFirstNum.Text
         Dim st2 As String = txtSecondNum.Text
+        'Checkpoint 1
+        If Not (IsNumeric(st1) And IsNumeric(st2)) Then
+            MessageBox.Show("Please type a numerical value")
+            Return
+        End If
+
+        'Checkpoint 2
+        If Not (num1 >= 0 And num2 >= 0) Then
+            MessageBox.Show("Please type a non-negative number")
+            Return
+        End If
 
         num1 = CDbl(st1)
         num2 = CDbl(st2)
@@ -25,6 +36,8 @@ Public Class Form1
         End If
 
         txtResult.Text = result
+
+
     End Sub
 
 End Class
